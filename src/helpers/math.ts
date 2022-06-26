07cd88ccf4ff5a869d905ad83deb6e5de8e5ea98
Kacper Kozak <kacperkk@gmail.com>
2022-06-27 01:57:49 +0200

    Add Vitest unit tests

--- src/helpers/math.ts
+++ src/helpers/math.ts
@@ -1,5 +1,4 @@
 import { Position } from '+game/types'
-import { seededRandom } from './random'
 
 export const distanceBetweenPoints = ([x1, y1]: Position, [x2, y2]: Position) =>
     Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
