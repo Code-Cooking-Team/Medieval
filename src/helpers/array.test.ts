1c6e8337bbd1b8747823330f4bd4cb033d5a82c5
Kacper Kozak <kacperkk@gmail.com>
2022-06-27 02:02:26 +0200

    Simplify tests

--- src/helpers/array.test.ts
+++ src/helpers/array.test.ts
@@ -1,13 +1,11 @@
 import { createArray, getPositionByIndex } from './array'
 
-describe('helpers/array', () => {
-    it('should create an array', () => {
-        const array = createArray(3)
-        expect(array).toEqual([0, 1, 2])
-    })
+it('should create an array', () => {
+    const array = createArray(3)
+    expect(array).toEqual([0, 1, 2])
+})
 
-    it('should get position by index', () => {
-        const item = getPositionByIndex(5, 3)
-        expect(item).toEqual([2, 1])
-    })
+it('should get position by index', () => {
+    const item = getPositionByIndex(5, 3)
+    expect(item).toEqual([2, 1])
 })
--- src/helpers/color.test.ts
+++ src/helpers/color.test.ts
@@ -1,15 +1,13 @@
 import { color0xToHex, colorHexTo0x } from './color'
 
-describe('helpers/color', () => {
-    it('should convert 0x to hex', () => {
-        const color = 0xff0000
-        const hex = color0xToHex(color)
-        expect(hex).toEqual('#ff0000')
-    })
+it('should convert 0x to hex', () => {
+    const color = 0xff0000
+    const hex = color0xToHex(color)
+    expect(hex).toEqual('#ff0000')
+})
 
-    it('should convert hex to 0x', () => {
-        const color = '#ff0000'
-        const hex = colorHexTo0x(color)
-        expect(hex).toEqual(0xff0000)
-    })
+it('should convert hex to 0x', () => {
+    const color = '#ff0000'
+    const hex = colorHexTo0x(color)
+    expect(hex).toEqual(0xff0000)
 })
