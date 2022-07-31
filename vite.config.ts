e40a1b60dad73fc16868342e094daf489c0592fc
Kacper Kozak <kacperkk@gmail.com>
2022-07-31 18:09:54 +0200

    Enable React fastRefresh

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     plugins: [
         tsconfigPaths(),
         react({
-            fastRefresh: false,
+            fastRefresh: true,
         }),
     ],
     server: {
