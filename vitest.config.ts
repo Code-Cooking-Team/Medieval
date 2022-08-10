2de35933aa388706e2a1d03895f3db676165295b
Kacper Kozak <kacperkk@gmail.com>
2022-08-10 12:22:55 +0200

    Change Vitest environment to happy-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         globals: true,
+        environment: 'happy-dom',
     },
     plugins: [tsconfigPaths() as any],
 })
