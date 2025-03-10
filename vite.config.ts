ba4c12739a353de38b90b4a0eabd594acfde3b64
Kacper Kozak <kacperkk@gmail.com>
2025-03-10 22:39:39 +0100

    chore: pnpm and IDE configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, Plugin } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
+const pluginSettings = {
+    configFilePath: './src/config.json',
+}
+
+const handyConfigPlugin: Plugin = {
+    name: 'handy-config-plugin',
+    configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+            console.log(req.url)
+            next()
+        })
+    },
+}
+
 export default defineConfig({
     base: '/Medieval/',
     assetsInclude: ['**/*.gltf', '**/*.hdr'],
     plugins: [
         tsconfigPaths(),
-        react({
-            fastRefresh: true,
-        }),
+        react({ fastRefresh: true }),
+        // Local plugin
+        handyConfigPlugin,
     ],
     server: {
         port: 3333,
